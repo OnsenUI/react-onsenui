8935bdd1fa3bee10862e6071578140ff7a805444
Patrick Klitzke <philologos14@gmail.com>
2016-04-14 14:58:42 +0900

    update docs of navigator

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -8,27 +8,18 @@ import BasicComponent from './BasicComponent.jsx';
  * [en]This is the main component for navigation. [/en]
  * [jp] どうしよう[/jp]
  * @example
- * class App extends React.Component {
- *  renderScene(route, navigator) {
- *     const props = route.props || {};
- *     props.navigator = navigator;
- *     return React.createElement(route.component, route.props);
- *  }
- *
- *  render() {
- *   return (
- *     <Navigator
- *       renderScene={this.renderScene}
- *       initialRoute={{
- *         component: Examples,
- *         props: {
- *           key: 'examples'
- *         }
- *       }}
- *     />
- *   );
- *  }
- *}
+  <Navigator
+    renderScene={(route, navigator) =>
+     <MyPage
+       title={route.title}
+       onPop={() => navigator.popPage()}
+       />
+    }
+    initialRoute={{
+        title: 'First Page'
+    }} />
+   }
+ }
  */
 class Navigator extends BasicComponent {
   constructor(props) {
@@ -60,6 +51,19 @@ class Navigator extends BasicComponent {
     return this.resetPageStack([route], options);
   }
 
+  /**
+   * @method resetPageStack
+   * @signature resetPageStack(route, options = {})
+   * @param {Array} [routes]
+   *   [en] The routes that the navigator should be reset to.[/en]
+   *   [ja] どうしよう [/ja]
+   * @return {Promise}
+   *   [en]Promise which resolves to the revealed page.[/en]
+   *   [ja]明らかにしたページを解決するPromiseを返します。[/ja]
+   * @description
+   *   [en]Rests the current page[/en]
+   *   [ja]どうしよう[/ja]
+   */
   resetPageStack(routes, options = {}) {
     return new Promise((resolve) => {
       var lastRoute = routes[routes.length - 1];
@@ -79,6 +83,19 @@ class Navigator extends BasicComponent {
     });
   }
 
+  /**
+   * @method pushPage
+   * @reset pushPage(route, options = {})
+   * @param {Array} [routes]
+   *   [en] The routes that the navigator should push to.[/en]
+   *   [ja] どうしよう [/ja]
+   * @return {Promise}
+   *   [en]Promise which resolves to the revealed page.[/en]
+   *   [ja]明らかにしたページを解決するPromiseを返します。[/ja]
+   * @description
+   *   [en]Pushes a page to the page stack[/en]
+   *   [ja]どうしよう[/ja]
+   */
   pushPage(route, options = {}) {
     return new Promise((resolve) => {
       var newPage = this.props.renderScene(route, navigator);
@@ -91,6 +108,16 @@ class Navigator extends BasicComponent {
     });
   }
 
+  /**
+   * @method popPage
+   * @reset popPage(route, options = {})
+   * @return {Promise}
+   *   [en]Promise which resolves to the revealed page.[/en]
+   *   [ja]明らかにしたページを解決するPromiseを返します。[/ja]
+   * @description
+   *   [en]Pops a page out of the page stack[/en]
+   *   [ja]どうしよう[/ja]
+   */
   popPage(options = {}) {
     return this.refs.navi._popPage(options, this.update.bind(this), this.pages)
       .then(
@@ -137,8 +164,41 @@ class Navigator extends BasicComponent {
 }
 
 Navigator.propTypes = {
+  /**
+   * @name renderScene
+   * @type function
+   * @required true
+   * @defaultValue null
+   * @description
+   *  [en] This function takes the current route object as a parameter and  creates returns a react componen.[/en]
+   *  [jp] どうしよう[/jp]
+   */
   renderScene: React.PropTypes.func.isRequired,
+  /**
+   * @name initialRoutes
+   * @type array
+   * @required false
+   * @defaultValue null
+   * @description
+   *  [en] This array contains the initial routes from the navigator,
+   *  which will be used to render the initial pages in the renderScene method.
+   *  [/en]
+   *  [jp] どうしよう[/jp]
+   */
   initialRoutes: React.PropTypes.array,
+
+  /**
+   * @name initialRoute
+   * @type object
+   * @required false
+   * @defaultValue null
+   * @description
+   *  [en] This array contains the initial route of the navigator,
+   *  which will be used to render the initial pages in the
+   *  renderScene method.
+   *  [/en]
+   *  [jp] どうしよう[/jp]
+   */
   initialRoute: React.PropTypes.object
 };
 
