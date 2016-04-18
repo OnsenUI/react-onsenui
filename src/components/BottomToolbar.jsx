ac2e3ad4feb9707d58cee85cc098fa5a50a9b936
Patrick Klitzke <philologos14@gmail.com>
2016-04-18 13:59:15 +0900

    modified bottom toolbar

--- src/components/BottomToolbar.jsx
+++ src/components/BottomToolbar.jsx
@@ -1,9 +1,14 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
+import React from 'react';
 
 class BottomToolbar extends SimpleWrapper {
   _getDomNodeName() {
     return 'ons-buttom-toolbar';
   }
+};
+
+BottomToolbar.propTypes = {
+  modifier: React.PropTypes.string
 };
 
 export default BottomToolbar;
