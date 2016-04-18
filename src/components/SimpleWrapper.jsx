3605c4486ed4b7e7e139be71f398fe2602d5e7dc
Patrick Klitzke <philologos14@gmail.com>
2016-04-18 15:59:51 +0900

    add ripple property

--- src/components/SimpleWrapper.jsx
+++ src/components/SimpleWrapper.jsx
@@ -3,10 +3,14 @@ import BasicComponent from './BasicComponent.jsx';
 
 class SimpleWrapper extends BasicComponent {
   render() {
-    var {disabled, ...others} = this.props;
+    var {disabled, ripple, ...others} = this.props;
 
     if (disabled) {
       others.disabled = 'disabled';
+    }
+
+    if (ripple) {
+      others.ripple = true;
     }
 
     return React.createElement(this._getDomNodeName(), others, this.props.children);
