085cb39abeafff70316ff2c11480baa7bdb86711
Patrick Klitzke <philologos14@gmail.com>
2016-04-18 12:37:00 +0900

    update Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
+import React from 'react';
 
 class Button extends SimpleWrapper {
   _getDomNodeName() {
     return 'ons-button';
   }
+};
+
+Button.propTypes = {
+  modifier: React.PropTypes.string,
+  disabled: React.PropTypes.bool,
+  ripple: React.PropTypes.bool,
+  onClick: React.PropTypes.func
 };
 
 export default Button;
