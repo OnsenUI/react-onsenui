5f9b11800179db8e3405a66132fae974e1635a43
Patrick Klitzke <philologos14@gmail.com>
2016-04-18 13:08:39 +0900

    some description for the back-button

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,5 +1,15 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
 
+/**
+ * @original ons-back-button
+ * @category page
+ * @description
+ * [en] Back button component for Toolbar. It enables to automatically to pop the top pae of the navigator. When only presented with one page, the button is hidden automatically.  [/en]
+ * [jp] どうしよう[/jp]
+ * @example
+  <BackButton> Back </BackButton>
+ */
+
 class BackButton extends SimpleWrapper {
   _getDomNodeName() {
     return 'ons-back-button';
