39802b60a6a22dc4f3f67c59e63aeaf4fb0f84cf
Patrick Klitzke <philologos14@gmail.com>
2016-04-19 14:33:32 +0900

    add toolbar types

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,4 +1,5 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
+import React from 'react';
 
 /**
  * I'm a toolbar!
@@ -7,6 +8,19 @@ class Toolbar extends SimpleWrapper {
   _getDomNodeName() {
     return 'ons-toolbar';
   }
+};
+
+Toolbar.propTypes = {
+  /**
+   * @name modifier
+   * @type string
+   * @description
+   *  [en]
+   *  Specify modifier name to specify custom styles. Optional.
+   *  [/en]
+   *  [jp] どうしよう[/jp]
+   */
+  modifier: React.PropTypes.string
 };
 
 export default Toolbar;
