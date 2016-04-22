ee9a43fe14356db25ecb8198279ad9d951e43c5e
Andreas Argelius <andreas@argeli.us>
2016-04-22 19:40:38 +0900

    Update generated files.

--- dist/react-onsenui.js
+++ dist/react-onsenui.js
@@ -1,4 +1,4 @@
-/*! react-onsenui v0.0.19 - Fri Apr 22 2016 19:38:30 GMT+0900 (JST) */
+/*! react-onsenui v0.0.19 - Fri Apr 22 2016 19:40:01 GMT+0900 (JST) */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react'), require('react-dom')) :
   typeof define === 'function' && define.amd ? define(['exports', 'react', 'react-dom'], factory) :
