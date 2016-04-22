aa23c46e9e36aca36ede75b710ccf115f0c55cbb
Patrick Klitzke <philologos14@gmail.com>
2016-04-22 14:05:53 +0900

    carousel item

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,23 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
+import React from 'react';
 
 class CarouselItem extends SimpleWrapper {
   _getDomNodeName() {
     return 'ons-carousel-item';
   }
+};
+
+CarouselItem.propTypes = {
+  /**
+   * @name modifier
+   * @type string
+   * @description
+   *  [en]
+   *  Specify modifier name to specify custom styles. Optional.
+   *  [/en]
+   *  [jp] どうしよう[/jp]
+   */
+  modifier: React.PropTypes.string
 };
 
 export default CarouselItem;
