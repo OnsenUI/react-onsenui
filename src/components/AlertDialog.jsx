c25047b2ede8987f163c4ef45e30894eddd6896a
Patrick Klitzke <philologos14@gmail.com>
2016-04-26 12:07:19 +0900

    documentation for alert dialog

--- src/components/AlertDialog.jsx
+++ src/components/AlertDialog.jsx
@@ -5,8 +5,13 @@ import React from 'react';
  * @original ons-alert-dialog
  * @category dialog
  * @description
- * [en] Alert dialog that is displayed on top of the current screen. [/en]
- * [jp] どうしよう[/jp]
+ * @description
+ *   [en]
+ *     Alert dialog that is displayed on top of the current screen. Useful for displaying questions, warnings or error messages to the user. The title, content and buttons can be easily customized and it will automatically switch style based on the platform.
+ *
+ *     To use the element it can either be attached directly to the `<body>` element or dynamically created from a template using the `ons.createAlertDialog(template)` utility function and the `<ons-template>` tag.
+ *   [/en]
+ * [jp][/jp]
  * @example
    <AlertDialog isOpen={this.state.isOpen} onCancel={this.handleCancel.bind(this)} cancelable>
      <div className="alert-dialog-title">Warning!</div>
@@ -30,17 +35,139 @@ class AlertDialog extends BaseDialog {
 }
 
 AlertDialog.propTypes = {
+  /**
+   * @name onCancel
+   * @type function
+   * @required false
+   * @description
+   *  [en]
+   *  Called only if isCancelable is true. It will be called after tapping the background or by pressing the back button on Android devices.
+   *  [/en]
+   *  [jp] どうしよう[/jp]
+   */
   onCancel: React.PropTypes.func,
+
+  /**
+   * @name isOpen
+   * @type bool
+   * @required true
+   * @description
+   *  [en]
+   *  Indicates whether the dialog is open and shown.
+   *  [/en]
+   *  [jp] [/jp]
+   */
   isOpen: React.PropTypes.bool.isRequired,
+
+  /**
+   * @name isCancelable
+   * @type bool
+   * @required false
+   * @description
+   *  [en]
+   *  Specifies whether the dialog is cancelable or not.
+   *  A cancelable dialog will call onCancel  when tapping the background or or  pressing the back button on Android devices
+   *  [/en]
+   *  [jp] [/jp]
+   */
   isCancelable: React.PropTypes.bool,
+
+  /**
+   * @name isDisabled
+   * @type bool
+   * @required false
+   * @description
+   *  [en]
+   *  Specifies whether the dialog is disabled.
+   *  [/en]
+   *  [jp] [/jp]
+   */
   isDisabled: React.PropTypes.bool,
+
+  /**
+   * @name animation
+   * @type string
+   * @required false
+   * @description
+   *  [en]
+   *  The animation used when showing and hiding the dialog. Can be either `"none"` or `"default"`.
+   *  [/en]
+   *  [jp] [/jp]
+   */
   animation: React.PropTypes.string,
+
+  /**
+   * @name modifier
+   * @type string
+   * @required false
+   * @description
+   *  [en]The appearance of the dialog.[/en]
+   *  [jp] [/jp]
+   */
   modifier: React.PropTypes.string,
+
+  /**
+   * @name maskColor
+   * @type string
+   * @required false
+   * @description
+   *  [en]Color of the background mask. Default is "rgba(0, 0, 0, 0.2)"[/en]
+   *  [jp] [/jp]
+   */
   maskColor: React.PropTypes.string,
+
+  /**
+   * @name animationOptions
+   * @type object
+   * @required false
+   * @description
+   *  [en]Specify the animation's duration, delay and timing. E.g.  `{duration: 0.2, delay: 0.4, timing: 'ease-in'}`.[/en]
+   *  [jp] [/jp]
+   */
   animationOptions: React.PropTypes.object,
+
+  /**
+   * @name onPreShow
+   * @type function
+   * @required false
+   * @description
+   *  [en]
+   *  Called just before the alert dialog is displayed.
+   *  [/en]
+   *  [jp][/jp]
+   */
   onPreShow: React.PropTypes.func,
+
+  /**
+   * @name onPostShow
+   * @type function
+   * @required false
+   * @description
+   *  [en]
+   *  Called just after the alert dialog is displayed.
+   *  [/en]
+   *  [jp][/jp]
+   */
   onPostShow: React.PropTypes.func,
+
+  /**
+   * @name onPreHide
+   * @type function
+   * @required false
+   * @description
+   *  [en]Called just before the alert dialog is hidden.[/en]
+   *  [jp][/jp]
+   */
   onPreHide: React.PropTypes.func,
+
+  /**
+   * @name onPostHide
+   * @type function
+   * @required false
+   * @description
+   *  [en]Called just after the alert dialog is hidden.[/en]
+   *  [jp][/jp]
+   */
   onPostHide: React.PropTypes.func
 };
 
