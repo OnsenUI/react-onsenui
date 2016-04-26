931d28c2152e44eadb4a20e4b7c4e5a1719ce7b6
Patrick Klitzke <philologos14@gmail.com>
2016-04-26 16:51:27 +0900

    add button docs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
 import SimpleWrapper from './SimpleWrapper.jsx';
 import React from 'react';
+/**
+ * @original ons-button
+ * @category page
+ * @description
+ * [en] Button component. If you want to place a button in a toolbar, use `<ons-toolbar-button>` or `<ons-back-button>` instead.   Will automatically display as a Material Design button with a ripple effect on Android.
+ [/en]
+ * [jp][/jp]
+ * @example
+ * <Button modifier="large--cta">
+ *   Tap Me
+ * </Button>
+ */
 
 class Button extends SimpleWrapper {
   _getDomNodeName() {
@@ -8,10 +20,37 @@ class Button extends SimpleWrapper {
 };
 
 Button.propTypes = {
+  /**
+   * @name modifier
+   * @type string
+   * @required false
+   * @description
+   *  [en]The appearance of the button.[/en]
+   *  [jp] [/jp]
+   */
   modifier: React.PropTypes.string,
+
+  /**
+   * @name disabled
+   * @type bool
+   * @description
+   *  [en]
+   *  Specifies whether the button is disabled.
+   *  [/en]
+   *  [jp] [/jp]
+   */
   disabled: React.PropTypes.bool,
-  ripple: React.PropTypes.bool,
-  onClick: React.PropTypes.func
+
+  /**
+   * @name ripple
+   * @type bool
+   * @description
+   *  [en]
+   *  Specifies whether the button has a ripple effect.
+   *  [/en]
+   *  [jp] [/jp]
+   */
+  ripple: React.PropTypes.bool
 };
 
 export default Button;
