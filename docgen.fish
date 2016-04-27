a0b56fe4f0d1253375280d458dfe71a7199ca75a
Patrick Klitzke <philologos14@gmail.com>
2016-04-27 12:46:00 +0900

    update docs

--- docgen.fish
+++ docgen.fish
@@ -1,4 +1,4 @@
-for file in (ls ./src/components/ | grep -v 'reactUtil' | grep -v 'todo')
+for file in (ls ./src/components/ | grep -v 'reactUtil' | grep -v 'todo' | grep -v "BaseDialog" | grep -v "Util")
   echo "Generate $name"
   set name (echo $file | cut -d '.' -f1 )
   node ./scripts/react-docgen.js ./src/components/$file > docs/$name.json
