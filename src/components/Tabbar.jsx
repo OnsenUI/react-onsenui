f8203e52cbaa9cbc0567babbefccaae85cf778d3
Patrick Klitzke <philologos14@gmail.com>
2016-04-27 12:05:28 +0900

    tabbar documentation

--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 import BasicComponent from './BasicComponent.jsx';
 import Util from './Util.js';
 
+/**
+ * @original ons-tabbar
+ * @category tabbar
+ * @description
+ * [en] Component to display a tabbar on either the top or the bottom of a page.
+ * To define the tabs and the content the property renderTabs need to be implemented, that returns an array of tabs and their content. See the example for specifics. [/en]* [jp][/jp]
+ * @example
+ *   renderTabs(activeIndex, tabbar) {
+    return ;
+  }
+
+      <Page>
+        <Tabbar
+          onPreChange={() => console.log('preChange')}
+          onPostChange={() => console.log('postChange')}
+          onReactive={() => console.log('postChange')}
+          position='bottom'
+          renderTabs={() => [
+            {
+              content: <TabPage title="Home" active={activeIndex === 0} tabbar={tabbar} />,
+              tab: <Tab label="Home" icon="md-home" />
+            },
+            {
+              content: <TabPage title="Settings" active={activeIndex === 1} tabbar={tabbar} />,
+              tab: <Tab label="Settings" icon="md-settings" />
+            }]
+          }
+        />
+      </Page>
+ */
+
 class Tabbar extends BasicComponent {
 
   constructor(props) {
@@ -34,10 +65,42 @@ class Tabbar extends BasicComponent {
     this.setState({activeIndex: event.index});
   }
 
+  /**
+   * @method setActiveTab
+   * @signature setActiveTab(index, options)
+   * @param {Number} index
+   *   [en]Tab index.[/en]
+   *   [ja]タブのインデックスを指定します。[/ja]
+   * @param {Object} [options]
+   *   [en]Parameter object.[/en]
+   *   [ja]オプションを指定するオブジェクト。[/ja]
+   * @param {Boolean} [options.keepPage]
+   *   [en]If true the page will not be changed.[/en]
+   *   [ja]タブバーが現在表示しているpageを変えない場合にはtrueを指定します。[/ja]
+   * @param {String} [options.animation]
+   *   [en]Animation name. Available animations are `"fade"`, `"slide"` and `"none"`.[/en]
+   *   [ja]アニメーション名を指定します。`"fade"`、`"slide"`、`"none"`のいずれかを指定できます。[/ja]
+   * @description
+   *   [en]Show specified tab page. Animations and other options can be specified by the second parameter.[/en]
+   *   [ja]指定したインデックスのタブを表示します。アニメーションなどのオプションを指定できます。[/ja]
+   * @return {Promise}
+   *   [en]Resolves to the new page element.[/en]
+   *   [ja][/ja]
+   */
   setActiveTab(index, options) {
     this.refs.tabbar.setActiveTab(index, options);
   }
 
+  /**
+   * @method getActiveTabIndex
+   * @signature getActiveTabIndex()
+   * @return {Number}
+   *   [en]The index of the currently active tab.[/en]
+   *   [ja]現在アクティブになっているタブのインデックスを返します。[/ja]
+   * @description
+   *   [en]Returns tab index on current active tab. If active tab is not found, returns -1.[/en]
+   *   [ja]現在アクティブになっているタブのインデックスを返します。現在アクティブなタブがない場合には-1を返します。[/ja]
+   */
   getActiveTabIndex() {
     return this.refs.tabbar.getActiveTabIndex();
   }
@@ -67,13 +130,77 @@ class Tabbar extends BasicComponent {
 };
 
 Tabbar.propTypes = {
-  initialIndex: React.PropTypes.number.isRequired,
+  /**
+   * @name initialIndex
+   * @type number
+   * @description
+   *  [en] The index of the first tab to show.[/en]
+   *  [jp] [/jp]
+   */
+  initialIndex: React.PropTypes.number,
+
+  /**
+   * @name renderTabs
+   * @type function
+   * @description
+   *  [en] The index of the first tab to show.[/en]
+   *  [jp] [/jp]
+   */
   renderTabs: React.PropTypes.func.isRequired,
+
+  /**
+   * @name position
+   * @type string
+   * @description
+   *  [en] Tabbar's position. Available values are `"bottom"` and `"top"`. Use `"auto"` to choose position depending on platform (iOS bottom, Android top). [/en]
+   *  [jp] [/jp]
+   */
   position: React.PropTypes.string,
+
+  /**
+   * @name animation
+   * @type string
+   * @description
+   *  [en] Animation name. Available values are `"none"`, `"slide"` and `"fade"`. Default is `"none"`. [/en]
+   *  [jp] [/jp]
+   */
   animation: React.PropTypes.oneOf(['none', 'slide', 'fade']),
+
+  /**
+   * @name animationOptions
+   * @type object
+   * @required false
+   * @description
+   *  [en]Specify the animation's duration, delay and timing. E.g.  `{duration: 0.2, delay: 0.4, timing: 'ease-in'}`.[/en]
+   *  [jp] [/jp]
+   */
   animationOptions: React.PropTypes.object,
+
+  /**
+   * @name onPreChagne
+   * @type function
+   * @description
+   *  [en]Called just before the tab is changed.[/en]
+   *  [jp] [/jp]
+   */
   onPreChange: React.PropTypes.func,
+
+  /**
+   * @name onPostChange
+   * @type function
+   * @description
+   *  [en]Called just after the tab is changed.[/en]
+   *  [jp] [/jp]
+   */
   onPostChange: React.PropTypes.func,
+
+  /**
+   * @name onPostChange
+   * @type function
+   * @description
+   *  [en]Called if the already open tab is tapped again.[/en]
+   *  [jp] [/jp]
+   */
   onReactive: React.PropTypes.func
 };
 
