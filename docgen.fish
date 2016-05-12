0b226df62379ea6b858df5c392367786e2e98896
Andreas Argelius <andreas@argeli.us>
2016-05-12 23:09:44 +0900

    Clean "docs" directory before generating

--- docgen.fish
+++ docgen.fish
@@ -1,3 +1,5 @@
+rm ./docs/*.json
+
 for file in (ls ./src/components/ | grep -v 'reactUtil' | grep -v 'todo' | grep -v "BaseDialog" | grep -v "Util")
   echo "Generate $name"
   set name (echo $file | cut -d '.' -f1 )
