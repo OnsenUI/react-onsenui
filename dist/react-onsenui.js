2c106493f1a57f9ad2e82b402f3b8b6a92ff5c1b
Patrick Klitzke <philologos14@gmail.com>
2016-06-10 13:27:10 +0900

    Update generated files.

--- dist/react-onsenui.js
+++ dist/react-onsenui.js
@@ -1,4 +1,4 @@
-/*! react-onsenui v0.4.0 - Fri Jun 10 2016 13:17:33 GMT+0900 (JST) */
+/*! react-onsenui v0.4.1 - Fri Jun 10 2016 13:27:05 GMT+0900 (JST) */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react'), require('react-dom')) :
   typeof define === 'function' && define.amd ? define(['exports', 'react', 'react-dom'], factory) :
