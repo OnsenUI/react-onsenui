7c77be4570584a674091c019a6253f067298ad1b
Andreas Argelius <andreas@argeli.us>
2016-07-26 16:01:08 +0900

    Update SpeedDial example to use renderFixed prop

--- demo/examples/SpeedDial.js
+++ demo/examples/SpeedDial.js
@@ -32,22 +32,27 @@ export default class extends React.Component {
     );
   }
 
+  renderFixed() {
+    return (
+      <SpeedDial disabled={false} direction='up' onClick={() => console.log('test1')} position='bottom right'>
+        <Fab>
+          <Icon
+            icon='fa-twitter'
+            size={26}
+            fixedWidth={false}
+            style={{verticalAlign: 'middle'}} />
+        </Fab>
+        <SpeedDialItem onClick={() => console.log('speed A')}> A </SpeedDialItem>
+        <SpeedDialItem onClick={() => console.log('speed B')}> B </SpeedDialItem>
+        <SpeedDialItem onClick={() => console.log('speed C')}> C </SpeedDialItem>
+        <SpeedDialItem onClick={() => console.log('speed D')}> D </SpeedDialItem>
+      </SpeedDial>
+    );
+  }
+
   render() {
     return (
-      <Page renderToolbar={this.renderToolbar}>
-        <SpeedDial disabled={false} direction='right' onClick={() => console.log('test1')} position='left bottom'>
-          <Fab>
-            <Icon
-              icon='fa-twitter'
-              size={26}
-              fixedWidth={false}
-              style={{verticalAlign: 'middle'}} />
-          </Fab>
-          <SpeedDialItem onClick={() => console.log('speed A')}> A </SpeedDialItem>
-          <SpeedDialItem onClick={() => console.log('speed B')}> B </SpeedDialItem>
-          <SpeedDialItem onClick={() => console.log('speed C')}> C </SpeedDialItem>
-          <SpeedDialItem onClick={() => console.log('speed D')}> D </SpeedDialItem>
-        </SpeedDial>
+      <Page renderFixed={this.renderFixed} renderToolbar={this.renderToolbar}>
       </Page>
     );
   }
