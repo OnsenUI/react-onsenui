26f60666609a47da15f23ad391695cc5daf9adad
Andreas Argelius <andreas@argeli.us>
2016-10-05 13:18:40 +0900

    Fix BottomToolbar docs

--- src/components/BottomToolbar.jsx
+++ src/components/BottomToolbar.jsx
@@ -5,10 +5,10 @@ import React from 'react';
  * @original ons-bottom-toolbar
  * @category page
  * @description
- * [en] Back button component for Toolbar. It enables to automatically to pop the top page of the navigator. When only presented with one page, the button is hidden automatically.  [/en]
+ * [en]Toolbar component that is positioned at the bottom of the page.[/en]
  * [jp][/jp]
  * @example
- *<BottomToolbar modifier="material"> Content </BottomToolbar>
+ * <BottomToolbar modifier="material"> Content </BottomToolbar>
  */
 class BottomToolbar extends SimpleWrapper {
   _getDomNodeName() {
@@ -21,9 +21,7 @@ BottomToolbar.propTypes = {
    * @name modifier
    * @type string
    * @description
-   *  [en]
-   *  Specify modifier name to specify custom styles. Optional.
-   *  [/en]
+   *  [en]Specify modifier name to specify custom styles. Optional.[/en]
    *  [jp][/jp]
    */
   modifier: React.PropTypes.string
